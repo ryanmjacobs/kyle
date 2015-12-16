c6a9b86dc6b5cb4332a28de29f918d47b475c494
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-12-15 22:14:26 -0800

    add ga linkid

--- js/ga.js
+++ js/ga.js
@@ -4,4 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-56980568-3', 'auto');
+ga('require', 'linkid');
 ga('send', 'pageview');
