47c51d66762618630f70241ee541631da714543d
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-11-28 02:23:58 -0800

    lame cold domen joke

---
+++ js/hide.js
@@ -0,0 +1,23 @@
+let cover = document.createElement("div");
+document.body.appendChild(cover);
+
+cover.style.position = "absolute";
+cover.style.top = "0px";
+cover.style.left = "0px";
+cover.style.width = "98vw";
+cover.style.height = "100vh";
+cover.style.zIndex = 99;
+cover.style.backgroundColor = "white";
+cover.style.color = "black"
+cover.style.padding = "1vw";
+
+cover.innerHTML =
+    'What\'s better than a cold domen?<br><br>' +
+    '<a href="#" id="ahotdomen">a hot domen</a>';
+
+let linky = document.getElementById("ahotdomen");
+linky.style.color = "blue";
+linky.style.paddingLeft = "2em";
+linky.onclick = function() {
+    document.body.removeChild(cover);
+};
